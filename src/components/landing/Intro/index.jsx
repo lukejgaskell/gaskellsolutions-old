b2af41032473e61ff733477931d56f91e497278e
Luke Gaskell <ljgaskell@liberty.edu>
2021-05-08 12:26:45 -0400

    updated main screen text

--- src/components/landing/Intro/index.jsx
+++ src/components/landing/Intro/index.jsx
@@ -1,30 +1,29 @@
-import { Button, Container } from 'components/common';
-import { Details, IntroWrapper, Thumbnail, Wrapper } from './styles';
-import React, { useContext } from 'react';
+import { Button, Container } from 'components/common'
+import { Details, IntroWrapper, Thumbnail, Wrapper } from './styles'
+import React, { useContext } from 'react'
 
-import AnchorLink from 'react-anchor-link-smooth-scroll';
-import { Header } from 'components/theme';
-import { ThemeContext } from 'providers/ThemeProvider';
-import dev from 'assets/illustrations/dev.svg';
+import AnchorLink from 'react-anchor-link-smooth-scroll'
+import { Header } from 'components/theme'
+import { ThemeContext } from 'providers/ThemeProvider'
+import dev from 'assets/illustrations/dev.svg'
 
 export const Intro = () => {
-  const { theme } = useContext(ThemeContext);
+  const { theme } = useContext(ThemeContext)
 
   return (
     <Wrapper>
       <Header />
       <IntroWrapper as={Container}>
         <Details theme={theme}>
-          <h1>Hi There!</h1>
-          <h4>I’m Luke and I’m a Full Stack Engineer!</h4>
+          <h4>I’m Luke and I’m a Full Stack Software Engineer!</h4>
           <Button as={AnchorLink} href="#contact">
             Contact Me
           </Button>
         </Details>
         <Thumbnail>
-          <img src={dev} alt="I’m John and I’m a JAMStack engineer!" />
+          <img src={dev} alt="I’m Luke and I’m a Full Stack Software Engineer!" />
         </Thumbnail>
       </IntroWrapper>
     </Wrapper>
-  );
-};
+  )
+}
